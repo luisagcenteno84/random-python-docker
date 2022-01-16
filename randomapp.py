e4acb54771a41464c07ee89e070beb7418408811
luisagcenteno84 <luisagcenteno@gmail.com>
2022-01-15 23:31:50 -0700

    edit string

--- randomapp.py
+++ randomapp.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def rannum():
     x = random.randint(5000770880,9008800000)
     y = str(x)
-    return y
+    a = "Random Number Luis: " + y
+    return a
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=80)
